bdafc980ec8a4acb4f2b6dd6d08f09f35e10ebe8
Prince Kaushal <prince.kaushal@infoorigin.com>
2025-06-28 13:57:25 +0530

    changes

--- Frontend/src/pages/About.jsx
+++ Frontend/src/pages/About.jsx
@@ -6,7 +6,7 @@ const About = () => {
   return (
     <div>
       <div className='text-2xl text-center pt-8 border-t'>
-        <Title text1={"About"} text2={"US"} />
+        <Title text1={"About"} text2={"US"} />   
       </div>
 
       <div className='my-10 flex flex-col md:flex-row gap-16'>
