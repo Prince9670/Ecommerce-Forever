0824c01a829c505094e6f95044332857cc8e8aeb
Prince Kaushal <95558284+Prince9670@users.noreply.github.com>
2025-06-29 22:54:36 +0530

    Backend update

--- Backend/server.js
+++ Backend/server.js
@@ -1,18 +1,24 @@
 import express from 'express'
 import cors from 'cors'
 import 'dotenv/config'
-
+import connectCloudinary from './config/cloudinary.js'
+import connectDB from './config/mongodb.js'
+import userRouter from './routes/userRoute.js'
 // App config
 
 const app = express()
 const port = process.env.PORT || 4000
+connectDB()
+connectCloudinary()
 
 
 // middleware 
 app.use(express.json())
-app.use(cors())
+app.use(cors()) 
 
 // app endpoints 
+app.use('/api/users', userRouter)
+
 app.get('/' ,(req,res)=>{
     res.send("API Working")
 })
